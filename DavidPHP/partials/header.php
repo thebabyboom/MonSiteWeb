334b536e7a4124db4d9cf4d4cafeb2157584e1a2
David <thebabyboom@gmail.com>
2018-04-02 07:26:16 +0200

    modification des css

--- DavidPHP/partials/header.php
+++ DavidPHP/partials/header.php
@@ -4,8 +4,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<meta name="description" content="Site personnel de babyboom" />
 	<meta name="keywords" content="babyboom, informatique, programmation, hesit" />
-  <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
-  <link title="test" type="text/css" rel="stylesheet" href="bootstrap/css/bootstrap.css" media="screen" /> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <!--<link title="test" type="text/css" rel="stylesheet" href="bootstrap/css/bootstrap.css" media="screen" /> -->
 	<title>Mon site personnel</title>
  </head>
 
